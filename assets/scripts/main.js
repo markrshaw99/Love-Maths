a67582efb0da656dd790e114fca4ca43f5a38d32
markrshaw99 <markshaw1999@gmail.com>
2025-06-11 11:07:57 +0100

    Craeted JS Sctructure

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivideQuestion() {
+
+}
+
